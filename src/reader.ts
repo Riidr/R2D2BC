be7a0305678ac442434aea2a7f8bc182f4424d77
Johannes Deva-Koch <jk@pubfront.com>
2025-06-16 09:39:53 +0200

    Enable highlights for FXL

--- src/reader.ts
+++ src/reader.ts
@@ -989,7 +989,7 @@ function updateConfig(
   // Some settings must be disabled for fixed-layout publications
   // maybe we should warn the user we are disabling them here.
   if (publication.isFixedLayout) {
-    rights.enableAnnotations = false;
+    // rights.enableAnnotations = false;
     // rights.enableSearch = false;
     rights.enableTTS = false;
     rights.enableDefinitions = false;
